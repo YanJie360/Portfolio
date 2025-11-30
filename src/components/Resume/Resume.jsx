791ced46ab6d60394c3905cd2cf8e7ce925903c4
Liauw Yan Jie <s10267962@connect.edu.sg>
2025-11-30 21:38:23 +0800

    Resume carousel

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col, Carousel } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import cl_pdf from "../../Assets/../Assets/LIAUWYANJIE_CHINESE CV.pdf";
@@ -11,53 +11,63 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
+  const [index, setIndex] = useState(0); // 0: Chinese, 1: English
 
   useEffect(() => {
-    setWidth(window.innerWidth);
+    function onResize() {
+      setWidth(window.innerWidth);
+    }
+    onResize();
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
   }, []);
 
+  const handleSelect = (selectedIdx) => {
+    setIndex(selectedIdx);
+  };
+
+  const currentFile = index === 0 ? cl_pdf : en_pdf;
+
   return (
-    <div>
-      <Container fluid className="resume-section">
-        <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={cl_pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download Chinese CV
+    <Container fluid className="resume-section">
+      <Particle />
+
+      <Row style={{ justifyContent: "center", position: "relative", paddingTop: 20 }}>
+        <Col xs="auto">
+          <Button variant="primary" href={currentFile} target="_blank" style={{ maxWidth: "250px" }}>
+            <AiOutlineDownload />&nbsp; Download CV
           </Button>
-        </Row>
+        </Col>
+      </Row>
 
-        <Row className="resume">
-          <Document file={cl_pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
+      <Row className="resume" style={{ justifyContent: "center", paddingTop: 20 }}>
+        <Col md={10}>
+          <Carousel activeIndex={index} onSelect={handleSelect} interval={null}>
+            <Carousel.Item>
+              <div className="d-flex justify-content-center">
+                <Document file={cl_pdf} className="d-flex justify-content-center">
+                  <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+                </Document>
+              </div>
+              <Carousel.Caption>
+                <h5>Chinese CV</h5>
+              </Carousel.Caption>
+            </Carousel.Item>
 
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={en_pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download English CV
-          </Button>
-        </Row>
-
-        <Row className="resume">
-          <Document file={en_pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-
-      </Container>
-    </div>
+            <Carousel.Item>
+              <div className="d-flex justify-content-center">
+                <Document file={en_pdf} className="d-flex justify-content-center">
+                  <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+                </Document>
+              </div>
+              <Carousel.Caption>
+                <h5>English CV</h5>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
